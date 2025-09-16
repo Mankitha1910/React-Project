a745f8120d8ab5ba06d1b843c3a4f4551ff82f35
M Ankitha <mankitha1910@gmail.com>
2025-09-16 18:47:58 +0530

    Update project files for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,9 +7,10 @@ import greetingCards from './Data.js'
 
 createRoot(document.getElementById('root')).render(
     <>
+    <BrowserRouter basename="/React-Project">
     <div className='header'><header><h1 className='head'>Trendy Fashion Greeting Cards</h1></header></div>
     <div className='container'>{greetingCards.map((item) => (<App item={item} />))}
 </div>
-
+    </BrowserRouter>
     </>
 )
